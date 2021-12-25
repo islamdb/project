12629946e4be2b3c5fba1a0df180723bee187449
rafyaa <rafy.social@gmail.com>
2021-12-25 22:16:39 +0700

    update

---
+++ database/migrations/2021_12_25_151421_alter_members_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->dropUnique('members_name_unique');
+            $table->unique(['user_id', 'name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
